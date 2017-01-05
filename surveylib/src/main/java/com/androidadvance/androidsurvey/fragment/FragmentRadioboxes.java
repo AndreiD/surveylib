916b7aeeba4d1c5a4c7be1a32cacbf1bd678c276
Sanjeev <sanjeev.b@gmx.de>
2017-01-05 19:05:19 +0100

    after press of the back key show previous view of 'FragmentRadioboxes' in order to view the prior checked option

--- surveylib/src/main/java/com/androidadvance/androidsurvey/fragment/FragmentRadioboxes.java
+++ surveylib/src/main/java/com/androidadvance/androidsurvey/fragment/FragmentRadioboxes.java
@@ -9,7 +9,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -20,7 +19,6 @@ import com.androidadvance.androidsurvey.R;
 import com.androidadvance.androidsurvey.SurveyActivity;
 import com.androidadvance.androidsurvey.models.Question;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -31,68 +29,51 @@ public class FragmentRadioboxes extends Fragment {
     private Button button_continue;
     private TextView textview_q_title;
     private RadioGroup radioGroup;
-    private final ArrayList<RadioButton> allRb = new ArrayList<>();
-    private boolean at_leaset_one_checked = false;
-
+    private ViewGroup rootView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(
-                R.layout.fragment_radioboxes, container, false);
+        if(rootView == null){
+            rootView = (ViewGroup) inflater.inflate(
+                    R.layout.fragment_radioboxes, container, false);
 
-        button_continue = (Button) rootView.findViewById(R.id.button_continue);
-        textview_q_title = (TextView) rootView.findViewById(R.id.textview_q_title);
-        radioGroup = (RadioGroup) rootView.findViewById(R.id.radioGroup);
-        button_continue.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ((SurveyActivity) mContext).go_to_next();
-            }
-        });
+            button_continue = (Button) rootView.findViewById(R.id.button_continue);
+            textview_q_title = (TextView) rootView.findViewById(R.id.textview_q_title);
+            radioGroup = (RadioGroup) rootView.findViewById(R.id.radioGroup);
+            button_continue.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ((SurveyActivity) mContext).go_to_next();
+                }
+            });
+
+            button_continue.setVisibility(View.GONE);
+            mContext = getActivity();
+            q_data = (Question) getArguments().getSerializable("data");
+            textview_q_title.setText(q_data.getQuestionTitle());
+
+            fillRadioGroup();
+        }
 
         return rootView;
     }
 
-    private void collect_data() {
+    private void collect_data(String the_choice) {
 
-        //----- collection & validation for is_required
-        String the_choice = "";
-        at_leaset_one_checked = false;
-        for (RadioButton rb : allRb) {
-            if (rb.isChecked()) {
-                at_leaset_one_checked = true;
-                the_choice = rb.getText().toString();
-            }
-        }
-
-        if (the_choice.length() > 0) {
+        if (!the_choice.isEmpty()) {
             Answers.getInstance().put_answer(textview_q_title.getText().toString(), the_choice);
         }
 
-
         if (q_data.getRequired()) {
-            if (at_leaset_one_checked) {
-                button_continue.setVisibility(View.VISIBLE);
-            } else {
-                button_continue.setVisibility(View.GONE);
-            }
+            button_continue.setVisibility(View.VISIBLE);
+        } else {
+            button_continue.setVisibility(View.GONE);
         }
 
     }
 
-
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-
-        mContext = getActivity();
-        q_data = (Question) getArguments().getSerializable("data");
-
-        textview_q_title.setText(q_data.getQuestionTitle());
-
-
+    private void fillRadioGroup() {
         List<String> qq_data = q_data.getChoices();
         if (q_data.getRandomChoices()) {
             Collections.shuffle(qq_data);
@@ -104,26 +85,19 @@ public class FragmentRadioboxes extends Fragment {
             rb.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
             rb.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
             radioGroup.addView(rb);
-            allRb.add(rb);
+        }
 
-            rb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    collect_data();
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                RadioButton checkedRadioButton = (RadioButton) group.findViewById(checkedId);
+                boolean isChecked = checkedRadioButton.isChecked();
+
+                if (isChecked) {
+                    collect_data(checkedRadioButton.getText().toString());
                 }
-            });
-        }
-
-        if (q_data.getRequired()) {
-            if (at_leaset_one_checked) {
-                button_continue.setVisibility(View.VISIBLE);
-            } else {
-                button_continue.setVisibility(View.GONE);
             }
-        }
 
-
+        });
     }
-
-
 }
